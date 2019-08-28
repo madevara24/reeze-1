176fc423d501dbcaf7c5c779042d0b497bddbccd
zainokta <zainokta@gmail.com>
2019-08-28 21:01:30 +0700

    baru

--- model/user.go
+++ model/user.go
@@ -1,5 +1,29 @@
 package model
 
-func GetUser(){
-	
+import (
+	"log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type User struct {
+	gorm.Model
+	ID uint
+}
+
+func GetUser() {
+
+}
+
+func main() {
+	db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+	errCheck(err)
+	defer db.Close()
+}
+
+func errCheck(err error) {
+	if err != nil {
+		log.Fatalf("Error %v", err)
+	}
 }
