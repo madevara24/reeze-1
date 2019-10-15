4b3d67f6a4e5e9a88b253773e128a207facd7047
zainokta <zainokta@gmail.com>
2019-10-15 10:05:12 +0700

    change migration

---
+++ migration/database/seeds/ProjectMembersTableSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProjectMembersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('project_members')->insert([
+            [
+                'user_id' => 1,
+                'project_id' => 1,
+            ],
+            [
+                'user_id' => 1,
+                'project_id' => 2,
+            ],
+            [
+                'user_id' => 1,
+                'project_id' => 3,
+            ],
+            [
+                'user_id' => 2,
+                'project_id' => 1,
+            ],
+            [
+                'user_id' => 2,
+                'project_id' => 3,
+            ],
+            [
+                'user_id' => 3,
+                'project_id' => 3,
+            ],
+            [
+                'user_id' => 4,
+                'project_id' => 2,
+            ],
+            [
+                'user_id' => 5,
+                'project_id' => 1,
+            ],
+            [
+                'user_id' => 5,
+                'project_id' => 2,
+            ],
+            [
+                'user_id' => 7,
+                'project_id' => 3,
+            ],
+            [
+                'user_id' => 8,
+                'project_id' => 2,
+            ],
+        ]);
+    }
+}
