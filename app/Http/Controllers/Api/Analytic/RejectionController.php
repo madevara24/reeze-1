68948ec5be50119fb0d01462d409a399af0bff46
madevara24 <adityadevara91@gmail.com>
2019-12-19 12:17:40 +0700

    Add basic controller for rejection

---
+++ app/Http/Controllers/Api/Analytic/RejectionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Analytic;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RejectionController extends Controller
+{
+    public function show($id){
+        
+    }
+}
