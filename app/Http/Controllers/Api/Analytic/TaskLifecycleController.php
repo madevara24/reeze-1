238531ddd505214958e289143601a15c73f70d82
madevara24 <adityadevara91@gmail.com>
2019-12-24 16:58:05 +0700

    Finish getting the sprind dates

--- app/Http/Controllers/Api/Analytic/TaskLifecycleController.php
+++ app/Http/Controllers/Api/Analytic/TaskLifecycleController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Api\Analytic;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Model\Project;
+use App\Model\Card;
+use App\Model\CardLog;
 
 class TaskLifecycleController extends Controller
 {
@@ -13,5 +17,37 @@ class TaskLifecycleController extends Controller
 
         //Get cards from current project
         $card_ids = Card::where('project_id', $id)->pluck('id')->toArray();
+
+        //Set fisrt sprint start and end date
+        $date = new Carbon($project['created_at']);
+        $date->setWeekStartsAt($project['sprint_start_day']);
+        $date->setWeekEndsAt(
+            $project['sprint_start_day'] == 0 ? 6 : $project['sprint_start_day'] -1
+        );
+        $date->startOfWeek();
+
+        $sprint_dates = array();
+        $rejection_rate = array();
+
+        //Get all the sprint start and end date until current sprint
+        while(true){
+            $sprint_start_date = new Carbon($date);
+            $sprint_end_date = new Carbon($date->addDays($project['sprint_duration']));
+
+            array_push($sprint_dates, array($sprint_start_date, $sprint_end_date));
+
+            if(Carbon::now()->between($sprint_start_date, $sprint_end_date)){
+                break;
+            }
+        }
+
+        $sprint_dates = array_reverse($sprint_dates);
+        
+        //Cut the array to only get the last 5 iteration
+        while (count($sprint_dates) > 5) {
+            array_pop($sprint_dates);
+        }
+        $sprint_dates = array_reverse($sprint_dates);
+        dd($sprint_dates);
     }
 }
