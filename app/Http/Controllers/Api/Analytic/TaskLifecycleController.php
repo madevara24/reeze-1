e1dce76d170d029a0de3b737f3fdc96d09de6087
madevara24 <adityadevara91@gmail.com>
2019-12-25 14:28:52 +0700

    Finish task lifecycle controller

--- app/Http/Controllers/Api/Analytic/TaskLifecycleController.php
+++ app/Http/Controllers/Api/Analytic/TaskLifecycleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Analytic;
 
 use App\Http\Controllers\Controller;
+use App\Helpers\AnalyticHelper;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Model\Project;
@@ -11,43 +12,83 @@ use App\Model\CardLog;
 
 class TaskLifecycleController extends Controller
 {
+    
     public function show($id){
+        
+        $analytic_helper = new AnalyticHelper();
+
+        $task_lifecycle = array(
+            'planned' => array(),
+            'started' => array(),
+            'finished' => array(),
+            'accepted' => array(),
+            'rejected' => array()
+        );
+
         //Get current project
         $project = Project::where('id', $id)->first();
 
         //Get cards from current project
-        $card_ids = Card::where('project_id', $id)->pluck('id')->toArray();
+        $project_card_ids = Card::where('project_id', $id)->pluck('id')->toArray();
 
-        //Set fisrt sprint start and end date
-        $date = new Carbon($project['created_at']);
-        $date->setWeekStartsAt($project['sprint_start_day']);
-        $date->setWeekEndsAt(
-            $project['sprint_start_day'] == 0 ? 6 : $project['sprint_start_day'] -1
-        );
-        $date->startOfWeek();
+        //Get sprint dates for the last 5 sprints
+        $sprint_dates = $analytic_helper->getProjectSprintDates($project['id']);
 
-        $sprint_dates = array();
-        $rejection_rate = array();
+        //Get the ids of the cards that have activity 
+        $card_ids = CardLog::whereIn('card_id', $project_card_ids)
+            ->where('state', '!=' , 'created')
+            ->whereBetween('created_at', [$sprint_dates[0][0], $sprint_dates[count($sprint_dates) - 1][1]])
+            ->groupBy('card_id')->pluck('card_id')->toArray();
 
-        //Get all the sprint start and end date until current sprint
-        while(true){
-            $sprint_start_date = new Carbon($date);
-            $sprint_end_date = new Carbon($date->addDays($project['sprint_duration']));
+        foreach ($card_ids as $card_id) {
 
-            array_push($sprint_dates, array($sprint_start_date, $sprint_end_date));
+            $card_logs = CardLog::where('card_id', $card_id)
+                ->where('state', '!=' , 'created')
+                ->whereBetween('created_at', [$sprint_dates[0][0], $sprint_dates[count($sprint_dates) - 1][1]])
+                ->get()->toArray();
 
-            if(Carbon::now()->between($sprint_start_date, $sprint_end_date)){
-                break;
+            foreach ($card_logs as $key => $card_log) {
+
+                //Released logs don't need to be calculated since it's the last state
+                if($card_log['state'] == 'released')
+                    break;
+                    
+                //Search the state before and calculate the duration before the first log of the 5 sprints
+                if($key == 0 && $card_log['state'] != 'planned'){
+                    $state_before = CardLog::where('card_id', $card_log['card_id'])
+                        ->where('created_at', '<', $sprint_dates[0][0])
+                        ->latest('created_at')->first()->toArray();
+
+                    //Get the duration from the start of the sprint time to the first log, to be set as the duration of the state before
+                    $duration = $sprint_dates[0][0]->diffInMinutes($card_log['created_at']);
+                    
+                    array_push($task_lifecycle[$state_before['state']], $duration);
+                }
+
+                //If its the last log, count until now
+                if($key == count($card_logs) -1){
+                    $log_time = new Carbon($card_log['created_at']);
+                    $duration = $log_time->diffInMinutes(Carbon::now());
+
+                    array_push($task_lifecycle[$card_log['state']], $duration);
+
+                }else{
+                    $log_start_time = new Carbon($card_log['created_at']);
+                    $log_end_time = new Carbon($card_logs[$key + 1]['created_at']);
+
+                    $duration = $log_start_time->diffInMinutes($log_end_time);
+
+                    array_push($task_lifecycle[$card_log['state']], $duration);
+
+                }
             }
         }
-
-        $sprint_dates = array_reverse($sprint_dates);
         
-        //Cut the array to only get the last 5 iteration
-        while (count($sprint_dates) > 5) {
-            array_pop($sprint_dates);
+        foreach ($task_lifecycle as $state => $state_duration) {
+            
+            $task_lifecycle[$state] = array_sum($state_duration);
         }
-        $sprint_dates = array_reverse($sprint_dates);
-        dd($sprint_dates);
+
+        return $task_lifecycle;
     }
 }
