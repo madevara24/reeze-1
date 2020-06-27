c3f08cd66fcf9e3c454695b5bca699d470313f6c
zainokta <zainokta@gmail.com>
2020-06-27 18:57:30 +0700

    re get project on submit add project

--- resources/js/components/PageNoProjects.vue
+++ resources/js/components/PageNoProjects.vue
@@ -1,27 +1,41 @@
 <template>
-  <v-row>
-    <v-col cols="12" align="center">
-      <v-img
-        src="https://upload.wikimedia.org/wikipedia/commons/8/86/Solid_grey.svg"
-        height="300"
-        max-width="400"
-      ></v-img>
-    </v-col>
-    <v-col cols="12" align="center" class="mt-5 pt-5">
-      <h4 class="font-weight-bold">You don't have any project running right now.</h4>
-    </v-col>
-    <v-col
-      cols="12"
-      align="center"
-      class="py-0"
-    >Create your first project and start tracking your sprint development</v-col>
-    <v-col cols="12" align="center">
-      <a href="/create" style="color:#0A60C4;text-decoration:none">
-        <span class="caption font-weight-bold">START YOUR FIRST PROJECT</span>
-      </a>
-    </v-col>
-  </v-row>
+    <v-row>
+        <v-col cols="12" align="center">
+            <v-img
+                src="https://upload.wikimedia.org/wikipedia/commons/8/86/Solid_grey.svg"
+                height="300"
+                max-width="400"
+            ></v-img>
+        </v-col>
+        <v-col cols="12" align="center" class="mt-5 pt-5">
+            <h4 class="font-weight-bold">
+                You don't have any project running right now.
+            </h4>
+        </v-col>
+        <v-col cols="12" align="center" class="py-0"
+            >Create your first project and start tracking your sprint
+            development</v-col
+        >
+        <v-col cols="12" align="center">
+            <a @click="openDialog()" style="color:#0A60C4;text-decoration:none">
+                <span class="caption font-weight-bold"
+                    >START YOUR FIRST PROJECT</span
+                >
+            </a>
+        </v-col>
+    </v-row>
 </template>
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            dialog: false
+        };
+    },
+    methods: {
+        openDialog(){
+            this.$emit("update", true);
+        },
+    }
+};
 </script>
